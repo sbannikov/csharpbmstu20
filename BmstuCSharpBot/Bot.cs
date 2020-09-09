e207a32393f3709bf5d496a6cd09a4a04aad2e05
Bannikov Sergey <sbannikov@croc.ru>
2020-09-09 13:35:04 +0300

    Лекция 2 - каркас бота в виде консольного приложения

---
+++ BmstuCSharpBot/Bot.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace BmstuCSharpBot
+{
+    /// <summary>
+    /// Бот для Telegram
+    /// </summary>
+    internal class Bot
+    {
+        /// <summary>
+        /// Клиент для Telegram
+        /// </summary>
+        private readonly TelegramBotClient client;
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        internal Bot()
+        {
+            client = new TelegramBotClient("410878642:AAEtUaY0FriWJIfJAy7RUI46WXn3agJ95YQ");
+            client.OnMessage += MessageProcessor;
+        }
+
+        /// <summary>
+        /// Обработчик события - принятое сообщение
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MessageProcessor(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+
+            Console.WriteLine($"{e.Message.Type} {e.Message.Text}");
+
+            // Имя метода, который надо вызвать
+            string name = $"{e.Message.Type}Processor";
+
+            // Поиск метода класса
+            MethodInfo method = GetType().GetMethod(name);
+
+            // Проверка на наличие метода
+            if (method == null)
+            {
+                // Метод не найден (имеет смысл записать в протокол)
+                client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты мне отправил {e.Message.Type}, но я пока этого не понимаю");
+            }
+            else
+            {
+                // Вызов метода с единственным параметром
+                method.Invoke(this, new object[] { e.Message });
+            }
+        }
+
+        /// <summary>
+        /// Обработка текстовых сообщений
+        /// </summary>
+        /// <param name="m"></param>
+        public void TextProcessor(Telegram.Bot.Types.Message m)
+        {
+            // Проверка на команду
+            if (m.Text.Substring(0, 1) == "/")
+            {
+                // Это команда - переходим к вызову метода команды
+                // Имя метода, который надо вызвать
+                string cmd = m.Text.Substring(1).ToLower();
+                string name = $"{cmd}Command";
+
+                // Поиск метода класса
+                MethodInfo method = GetType().GetMethod(name);
+
+                // Проверка на наличие метода
+                if (method == null)
+                {
+                    // команда не найдена (имеет смысл записать в протокол)
+                    client.SendTextMessageAsync(m.Chat.Id, $"Я пока не понимаю команду {cmd}");
+                }
+                else
+                {
+                    // Вызов метода с единственным параметром
+                    method.Invoke(this, new object[] { m });
+                }
+            }
+            else
+            {
+                client.SendTextMessageAsync(m.Chat.Id, $"Ты сказал мне: {m.Text}");
+            }
+        }
+
+        /// <summary>
+        /// Команда /start
+        /// </summary>
+        /// <param name="m"></param>
+        public void startCommand(Telegram.Bot.Types.Message m)
+        {
+            client.SendTextMessageAsync(m.Chat.Id, $"Привет, {m.Chat.FirstName}, рад знакомству!");
+        }
+
+        /// <summary>
+        /// Запуск бота
+        /// </summary>
+        internal void Run()
+        {
+            client.StartReceiving();
+        }
+    }
+}
---
+++ BmstuCSharpBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmstuCSharpBot
+{
+    /// <summary>
+    /// Класс для точки запуска приложения
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Точка запуска приложения
+        /// </summary>
+        /// <param name="args">Список параметров командной строки</param>
+        /// <returns>Всегда возвращает 0</returns>
+        static int Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.Run();
+            Console.WriteLine("Бот запущен. Для останова нажмите Enter");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
