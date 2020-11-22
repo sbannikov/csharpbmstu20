95198f3e1a0477a1631cece1b0a6d0e3a110533c
Банников Сергей <sbannikov@croc.ru>
2020-11-23 01:15:11 +0300

    Контрольный пример для РК2

--- IntermediateAssessment/Samples/Sample2.cs
+++ IntermediateAssessment/Samples/Sample2.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ServiceModel;
 
 namespace IntermediateAssessment.Samples
 {
@@ -10,11 +8,74 @@ namespace IntermediateAssessment.Samples
         private double[][] data;
         private int n;
         private int m;
-        public Matrix(int N, int M)
+
+        public Matrix() { }
+
+        // Инициализация матрицы заданного размера
+        public Matrix(int n, int m)
         {
-            n = N;
-            m = M;
-            data = new double[][] { new double[] { 1.0 } };
+            this.n = n;
+            this.m = m;
+            data = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = new double[m];
+                for (int j = 0; i < m; j++)
+                {
+                    data[i][j] = double.NaN;
+                }
+            }
+        }
+
+        // Создание матрицы с размером как у заданной матрицы
+        public Matrix(Matrix m)
+            : this(m.n, m.m)
+        {
+        }
+    }
+
+    [ServiceContract()]
+    public interface IExercise2
+    {
+        // Обратная матрица
+        [OperationContract()]        
+        Matrix Inversion(Matrix m);
+
+        // Определитель матрицы
+        [OperationContract()]
+        double Determinant(Matrix m);
+    }
+
+    [ServiceBehavior
+     (InstanceContextMode = InstanceContextMode.PerCall,
+      ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class Exercise2 : IExercise2
+    {
+        public double Determinant(Matrix m)
+        {
+            double result = double.NaN;
+            // Код вычисления определителя матрицы для простоты опущен
+            return result;
+        }
+
+        public Matrix Inversion(Matrix m)
+        {
+            Matrix result = new Matrix(m);
+            // Код вычисления обратной матрицы для простоты опущен
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host;
+            host = new ServiceHost(typeof (Exercise2));
+
+            host.Open();
+            Console.WriteLine("Сервис запущен");
+            Console.ReadLine();
         }
     }
 }
