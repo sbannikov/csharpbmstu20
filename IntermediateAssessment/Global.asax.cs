b15b12a2a6bcb864c9d3bbe7d9556b20ef1ac999
Банников Сергей <sbannikov@croc.ru>
2020-11-27 14:27:00 +0300

    Добавление глобального обработчика ошибок уровня приложения

--- IntermediateAssessment/Global.asax.cs
+++ IntermediateAssessment/Global.asax.cs
@@ -5,17 +5,34 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 
 namespace IntermediateAssessment
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        /// Протоколирование
+        /// </summary>
+        private Logger log = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Error += MvcApplication_Error;
+        }
+
+        /// <summary>
+        /// Обработка ошибок уровня приложения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MvcApplication_Error(object sender, EventArgs e)
+        {
+            log.Error(Server.GetLastError());
         }
     }
 }
